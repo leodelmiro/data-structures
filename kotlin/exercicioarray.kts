c384afd2c255c1009cca7827c103e2217263a555
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-11-18 13:14:15 -0300

    mxercicio aproduto de dois arrays

--- kotlin/exercicioarray.kts
+++ kotlin/exercicioarray.kts
@@ -17,3 +17,17 @@ fun findMaxConsecutiveOnes(nums: IntArray): Int {
 println(findMaxConsecutiveOnes(intArrayOf(1,1,0,1,1,1)))
 println(findMaxConsecutiveOnes(intArrayOf(1,0,1,1,0,1)))
 println("----------------------------")
+
+fun dotProductTwoArrays(nums1: IntArray, nums2: IntArray): Int {
+    var total = 0
+    for(i in nums1.indices) {
+        total += nums1[i] * nums2[i]
+    }
+
+    return total
+}
+
+println(dotProductTwoArrays(intArrayOf(1,0,0,2,3), intArrayOf(0,3,0,4,0)))
+println(dotProductTwoArrays(intArrayOf(0,1,0,0,0), intArrayOf(0,0,0,0,0)))
+println(dotProductTwoArrays(intArrayOf(0,1,0,0,2,0,0), intArrayOf(1,0,0,0,3,0,4)))
+println("----------------------------")
