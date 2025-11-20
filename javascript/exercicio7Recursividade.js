25946e2126baf9069231fb830b360b6305699096
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-11-20 12:36:29 -0300

    Exercicio recursividade de cauda

--- javascript/exercicio7Recursividade.js
+++ javascript/exercicio7Recursividade.js
@@ -25,3 +25,17 @@ console.log(factorial(1))
 console.log(factorial(2))
 console.log(factorial(3))
 console.log(factorial(4))
+
+function factorialComRecursividadeDeCauda(num, acumulador = 1) {
+    if(num === 0) {
+        return acumulador
+    }
+
+    return factorialComRecursividadeDeCauda(num - 1, num * acumulador)
+}
+
+console.log(factorialComRecursividadeDeCauda(0))
+console.log(factorialComRecursividadeDeCauda(1))
+console.log(factorialComRecursividadeDeCauda(2))
+console.log(factorialComRecursividadeDeCauda(3))
+console.log(factorialComRecursividadeDeCauda(4))
