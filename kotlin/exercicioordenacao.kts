9920fe4c14a9cb83c1bc5a2dd36fac8fc95a1687
Leonardo Delmiro <leodelmiroms@gmail.com>
2025-11-22 22:20:54 -0300

    Exercicio Insertion Sort

--- kotlin/exercicioordenacao.kts
+++ kotlin/exercicioordenacao.kts
@@ -16,8 +16,8 @@ fun bubbleSort(nums: IntArray): IntArray {
     return nums
 }
 
-println(bubbleSort(intArrayOf(20, 9, 86, -2, 16)).map { it.toString() }.reduce { acc, it -> "$acc, $it"})
-println(bubbleSort(intArrayOf(5, 4, 3, 2, 1)).map { it.toString() }.reduce { acc, it -> "$acc, $it"})
+println(bubbleSort(intArrayOf(20, 9, 86, -2, 16)).map { it.toString() }.reduce { acc, it -> "$acc, $it" })
+println(bubbleSort(intArrayOf(5, 4, 3, 2, 1)).map { it.toString() }.reduce { acc, it -> "$acc, $it" })
 
 // Bubble Sort Otimizado
 // Complexidade O (n²)
@@ -40,8 +40,8 @@ fun bubbleSortOtimizado(nums: IntArray): IntArray {
     return nums
 }
 
-println(bubbleSortOtimizado(intArrayOf(20, 9, 86, -2, 16)).map { it.toString() }.reduce { acc, it -> "$acc, $it"})
-println(bubbleSortOtimizado(intArrayOf(5, 4, 3, 2, 1)).map { it.toString() }.reduce { acc, it -> "$acc, $it"})
+println(bubbleSortOtimizado(intArrayOf(20, 9, 86, -2, 16)).map { it.toString() }.reduce { acc, it -> "$acc, $it" })
+println(bubbleSortOtimizado(intArrayOf(5, 4, 3, 2, 1)).map { it.toString() }.reduce { acc, it -> "$acc, $it" })
 
 
 // Selection Sort
@@ -68,5 +68,29 @@ fun selectionSort(nums: IntArray): IntArray {
     return nums
 }
 
-println(selectionSort(intArrayOf(20, 9, 86, -2, 16)).map { it.toString() }.reduce { acc, it -> "$acc, $it"})
-println(selectionSort(intArrayOf(5, 4, 3, 2, 1)).map { it.toString() }.reduce { acc, it -> "$acc, $it"})
+println(selectionSort(intArrayOf(20, 9, 86, -2, 16)).map { it.toString() }.reduce { acc, it -> "$acc, $it" })
+println(selectionSort(intArrayOf(5, 4, 3, 2, 1)).map { it.toString() }.reduce { acc, it -> "$acc, $it" })
+
+// Insertion Sort
+// Complexidade tempo O(n²)
+// Complexidade espaço O(1)
+// Ideal para poucos elementos
+
+fun insertionSort(nums: IntArray): IntArray {
+    for (i in 1 until nums.size) {
+        val aux = nums[i]
+        var j = i - 1
+
+        while (j >= 0 && nums[j] > aux) {
+            nums[j + 1] = nums[j]
+            j--
+        }
+
+        nums[j + 1] = aux
+    }
+
+    return nums
+}
+
+println(insertionSort(intArrayOf(20, 9, 86, -2, 16)).map { it.toString() }.reduce { acc, it -> "$acc, $it" })
+println(insertionSort(intArrayOf(5, 4, 3, 2, 1)).map { it.toString() }.reduce { acc, it -> "$acc, $it" })
